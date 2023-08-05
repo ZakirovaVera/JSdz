451efc00ff1696b90c17980bdb07f47567ee4fdc
ZakirovaVera <zakirovavk@gmail.com>
2023-08-05 17:01:45 +0500

    dz 12 задача 2

--- dz12/script.js
+++ dz12/script.js
@@ -36,3 +36,16 @@ book.displayInfo(); // "Title: BookName1, Author: Author1, Pages: 123"
 // // Пример использования класса
 // const student = new Student("John Smith", 16, "10th grade");
 // student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"
+
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    displayInfo() {
+        console.log(`"Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}"`);
+    }
+}
+const student = new Student("John Smith", 16, "10th grade");
+student.displayInfo(); // "Name: John Smith, Age: 16, Grade: 10th grade"
